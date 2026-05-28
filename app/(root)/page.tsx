2f04f463e9377513a28374ad7a3a483bff7bb5c0
ashish-254 <ashishkumarsahu254@gmail.com>
2026-05-28 07:55:33 +0530

    working on the backend

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,21 @@
-import PodcastCard from "@/components/PodcastCard";
-import { Button } from "@/components/ui/button";
-import { podcastData } from "@/constants";
+"use client";
 import React from "react";
-
+import PodcastCard from "@/components/PodcastCard";
+import { podcastData } from "@/constants";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 const Home = () => {
+  const tasks = useQuery(api.tasks.get);
   return (
     <div className="px-5 mt-9 flex flex-col gap-9">
       <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
+
+        <div className="flex min-h-screen flex-col items-center justify-between p-24 text-white">
+          {tasks?.map(({ _id, text }) => (
+            <div key={_id}>{text}</div>
+          ))}
+        </div>
 
         <div className="podcast_grid">
           {podcastData.map(({ id, description, imgURL, title }) => (
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
+import ConvexClerkProvider from "./providers/ConvexClerkProvider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Podcastr",
@@ -26,11 +19,49 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en">
+      <body className={inter.className}>
+        <ConvexClerkProvider>{children}</ConvexClerkProvider>
+      </body>
     </html>
   );
 }
+// import type { Metadata } from "next";
+// import { Geist, Geist_Mono } from "next/font/google";
+// import "./globals.css";
+// import ConvexClerkProvider from "./providers/ConvexClerkProvider";
+
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+// export const metadata: Metadata = {
+//   title: "Podcastr",
+//   description: "Generate your podcast by AI",
+//   icons: {
+//     icon: "/icons/logo.svg",
+//   },
+// };
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <html
+//       lang="en"
+//       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+//     >
+//       <body className="min-h-full flex flex-col">
+//         <ConvexClerkProvider>{children}</ConvexClerkProvider>
+//       </body>
+//     </html>
+//   );
+// }
